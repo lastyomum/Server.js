a9782fcceda9bbce38a1b0932eadfc850532eb65
Nev <106666128+lastyomum@users.noreply.github.com>
2024-03-25 10:57:36 +1000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,4 @@ menuOpen.addEventListener('click', function() {
     mbs.style.display = mbs.style.display == "block" ? "none" : "block";
 });
 
-window.location.href = "construction.html"
+//window.location.href = "construction.html"
